c8068cf21169459b2659914c13c784ad127f687f
Borozdov <dev@borozdov.ru>
2026-05-05 13:53:54 +0700

    Add Yandex.Metrika counter

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -86,6 +86,19 @@ export default (() => {
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
 
+        {/* Yandex.Metrika counter */}
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109047859', 'ym');
+          ym(109047859, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+        `}} />
+        <noscript><div><img src="https://mc.yandex.ru/watch/109047859" style={{position:"absolute", left:"-9999px"}} alt="" /></div></noscript>
+        {/* /Yandex.Metrika counter */}
+
         {css.map((resource) => CSSResourceToStyleElement(resource, true))}
         {js
           .filter((resource) => resource.loadTime === "beforeDOMReady")
